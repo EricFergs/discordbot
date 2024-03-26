2a88f67cbdebad6c4ca80717a130d35cd709f847
EricFergs <135307549+EricFergs@users.noreply.github.com>
2024-03-25 19:19:01 -0700

    finished caching

--- datacaching.py
+++ datacaching.py
@@ -1,6 +1,6 @@
 import json
 import requests
-from datetime import datetime
+from datetime import datetime, timezone
 
 
 def fetchdata(*, update: bool = False, json_cache: str, url: str):
@@ -9,22 +9,22 @@ def fetchdata(*, update: bool = False, json_cache: str, url: str):
     else:
         try:
             with open(json_cache, 'r') as file:
-                print("no new data")
                 json_data = json.load(file)
+                rotationtime = json_data['data']['regularSchedules']['nodes'][0]['endTime']
+                current_datetime_utc = datetime.now(timezone.utc)
+                currtime = current_datetime_utc.strftime('%Y-%m-%dT%H:%M:%SZ')
+                if (currtime > rotationtime):
+                    json_data = None
         except (FileNotFoundError, json.JSONDecodeError) as e:
             print(f'the error is {e}')
             json_data = None
     if not json_data:
         print("getting new data")
         json_data = requests.get(url).json()
-        currtime = datetime.now().replace(microsecond=0).isoformat()
-        json_data_with_time = {
-            'data': json_data,
-            'timestamp': currtime
-        }
         with open(json_cache, 'w') as file:
-            json.dump(json_data_with_time, file)
-    return json_data_with_time
+            json.dump(json_data, file)
+    return json_data
+
 
 if __name__ == "__main__":
     url = 'https://splatoon3.ink/data/schedules.json'
@@ -32,4 +32,4 @@ if __name__ == "__main__":
     data: dict = fetchdata(
         update=False, json_cache=json_cache, url=url)
 
-    print("done")
+    
