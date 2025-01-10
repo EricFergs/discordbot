ec8fbc73c8a51b0ddb2db74243ed225370975375
EricFergs <135307549+EricFergs@users.noreply.github.com>
2025-01-09 20:44:54 -0800

    Generated a secret key

--- splatnet/AES.py
+++ splatnet/AES.py
@@ -1,14 +1,21 @@
 from Crypto.Cipher import AES
 from secrets import token_bytes
+import os
+from dotenv import load_dotenv
+import base64
 
+load_dotenv(override=True)
 #Using a 256 bit key for extra security. My Database is farily small so I can afford the extra computation. 
-key = token_bytes(32)
 
-def encrypt(msg):
+#Key stored privately 
+key = base64.b64decode(os.getenv("AES_KEY"))
+
+
+def encrypt(token):
     cipher = AES.new(key, AES.MODE_GCM)  #Creates a cipher object and sets the mode to EAX
     nonce = cipher.nonce         #creates a number used onc
-    ciphertext, tag = cipher.encrypt_and_digest(msg.encode('ascii'))
-    return nonce, ciphertext, tag
+    ciphertext, tag = cipher.encrypt_and_digest(token.encode('ascii'))
+    return nonce, ciphertext, tag  #Nonce and tag will need to be stored for decryption
 
 def decrypt(nonce, ciphertext, tag):
     cipher = AES.new(key, AES.MODE_GCM, nonce=nonce)
@@ -19,10 +26,12 @@ def decrypt(nonce, ciphertext, tag):
     except:
         return False
 
-nonce, ciphertext, tag = encrypt(input('Enter a message: '))
+
+nonce, ciphertext, tag = encrypt("hello")
 plaintext = decrypt(nonce, ciphertext, tag)
-print(f'Cipher text: {ciphertext}')
 if not plaintext:
     print('Message is corrupted')
 else:
     print(f'Plain text: {plaintext}')
+
+ 
